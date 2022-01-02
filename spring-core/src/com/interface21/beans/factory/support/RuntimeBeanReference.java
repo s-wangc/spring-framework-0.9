efbacb37619765ece533dcdb5c40bd357f87096e
wangc <1969877890@qq.com>
2022-01-02 11:59:08 +0800

    了解代表ref节点的RuntimeBeanReference

--- spring-core/src/com/interface21/beans/factory/support/RuntimeBeanReference.java
+++ spring-core/src/com/interface21/beans/factory/support/RuntimeBeanReference.java
@@ -1,9 +1,8 @@
 package com.interface21.beans.factory.support;
 
 /**
- * Immutable placeholder class used for the value of a PropertyValue
- * object when it's a reference to another bean in this
- * factory to be resolved at runtime.
+ * 不可变占位符类, 当PropertyValue对象引用此工厂中要在运行时解析的另一个bean时,
+ * 用于该对象的值.
  *
  * @author Rod Johnson
  */
@@ -12,18 +11,18 @@ public class RuntimeBeanReference {
 	private final String beanName;
 
 	/**
-	 * Create a new RuntimeBeanReference to the given bean nam.
+	 * 为给定的bean name创建一个新的RuntimeBeanReference.
 	 *
-	 * @param beanName name of the target bean.
+	 * @param beanName 目标bean的名称
 	 */
 	public RuntimeBeanReference(String beanName) {
 		this.beanName = beanName;
 	}
 
 	/**
-	 * Return the target bean name.
+	 * 返回目标bean名称.
 	 *
-	 * @return the target bean name.
+	 * @return 目标bean的名称.
 	 */
 	public String getBeanName() {
 		return beanName;
