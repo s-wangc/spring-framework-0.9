35a599965f1506e0da0ba855e39b743a0374a79c
wangc <1969877890@qq.com>
2022-01-02 12:22:03 +0800

    了解代表子bean节点的ChildBeanDefinition

--- spring-core/src/com/interface21/beans/factory/support/ChildBeanDefinition.java
+++ spring-core/src/com/interface21/beans/factory/support/ChildBeanDefinition.java
@@ -9,11 +9,9 @@ import com.interface21.beans.PropertyValues;
 
 
 /**
- * Extension of BeanDefinition interface for beans whose
- * class is defined by their ancestry. PropertyValues
- * defined by the parent will also be "inherited",
- * although it's possible to override them by redefining
- * them in the property values associated with the child.
+ * 扩展BeanDefinition接口, 用于其类由祖先定义的bean. 由父项定义的
+ * PropertyValues也将是"inherited", 尽管可以通过在与子项相关联的属性
+ * 值中重新定义它们来覆盖它们.
  *
  * @author Rod Johnson
  * @version $Revision: 1.2 $
@@ -23,7 +21,7 @@ public class ChildBeanDefinition extends AbstractBeanDefinition {
 	private String parentName;
 
 	/**
-	 * Creates new BeanDefinition
+	 * 创建新的BeanDefinition
 	 */
 	public ChildBeanDefinition(String parentName, PropertyValues pvs, boolean singleton) {
 		super(pvs, singleton);
@@ -31,11 +29,9 @@ public class ChildBeanDefinition extends AbstractBeanDefinition {
 	}
 
 	/**
-	 * Return the name of the parent bean definition in
-	 * the current bean factory.
+	 * 返回当前bean工厂中父bean定义的名称.
 	 *
-	 * @return the name of the parent bean definition in
-	 * the current bean factory
+	 * @return 当前bean工厂中父bean定义的名称
 	 */
 	public String getParentName() {
 		return parentName;
@@ -49,4 +45,4 @@ public class ChildBeanDefinition extends AbstractBeanDefinition {
 			return false;
 		return super.equals(arg0) && ((ChildBeanDefinition) arg0).getParentName().equals(this.getParentName());
 	}
-} 
+}
