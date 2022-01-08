bb7e5af14f8438cb226a765cf61da3862d511e7e
wangc <1969877890@qq.com>
2022-01-08 16:58:22 +0800

    了解ApplicationContext接口所实现的MessageSource

--- spring-context/src/com/interface21/context/MessageSource.java
+++ spring-context/src/com/interface21/context/MessageSource.java
@@ -12,61 +12,52 @@ package com.interface21.context;
 import java.util.Locale;
 
 /**
- * Interface to be implemented by objects that can resolve messages.
- * This enables parameterization and internationalization of messages.
+ * 将由可以解析message的对象实现的接口.
+ * 这可以实现message里的参数化和国际化.
  *
  * @author Rod Johnson
  */
 public interface MessageSource {
 
-	/**
-	 * Try to resolve the message.Return default message if no message
-	 * was found.
-	 *
-	 * @param code           code to lookup up, such as 'calculator.noRateSet'.
-	 *                       Users of this class are encouraged to base message names
-	 *                       on the relevant fully qualified class name, thus avoiding
-	 *                       conflict and ensuring maximum clarity.
-	 * @param args           Array of arguments that will be filled in for params within
-	 *                       the message (params look like "{0}", "{1,date}", "{2,time}" within a message),
-	 *                       or null if none.
-	 * @param locale         Locale in which to do lookup
-	 * @param defaultMessage String to return if the lookup fails
-	 * @return a resolved message if the lookup is successful;
-	 * otherwise return the default message passed as a parameter
-	 * @see <a href="http://java.sun.com/j2se/1.3/docs/api/java/text/MessageFormat.html">java.text.MessageFormat</a>
-	 */
-	String getMessage(String code, Object args[], String defaultMessage, Locale locale);
+    /**
+     * 尝试解析message. 如果找不到message, 则返回默认message.
+     *
+     * @param code           要查找的code, 例如'calculator.noRateSet'. 鼓励此类用户将message
+     *                       名称基于相关的完全限定类名, 从而避免冲突并确保最大程度的清晰性.
+     * @param args           将填充消息中的参数的参数数组(参数在消息中看起来像"{0}", "{1,date}", "{2,time}"),
+     *                       如果没有则为null.
+     * @param locale         要在其中进行查找的locale
+     * @param defaultMessage 查找失败时返回的字符串
+     * @return 如果查找成功, 则返回已解析的消息; 否则返回作为参数传递的默认消息;
+     * @see <a href="http://java.sun.com/j2se/1.3/docs/api/java/text/MessageFormat.html">java.text.MessageFormat</a>
+     */
+    String getMessage(String code, Object args[], String defaultMessage, Locale locale);
 
-	/**
-	 * Try to resolve the message. Treat as an error if the message can't
-	 * be found.
-	 *
-	 * @param code   code to lookup up, such as 'calculator.noRateSet'
-	 * @param args   Array of arguments that will be filled in for params within
-	 *               the message (params look like "{0}", "{1,date}", "{2,time}" within a message),
-	 *               or null if none.
-	 * @param locale Locale in which to do lookup
-	 * @return message
-	 * @throws NoSuchMessageException not found in any locale
-	 * @see <a href="http://java.sun.com/j2se/1.3/docs/api/java/text/MessageFormat.html">java.text.MessageFormat</a>
-	 */
-	String getMessage(String code, Object args[], Locale locale) throws NoSuchMessageException;
+    /**
+     * 尝试解析该message. 如果找不到message,.则视为错误.
+     *
+     * @param code   要查找的code, 例如'calculator.noRateSet'
+     * @param args   将填充消息中的参数的参数数组(参数在消息中看起来像"{0}", "{1,date}", "{2,time}"),
+     *               如果没有则为null.
+     * @param locale 要在其中进行查找的locale
+     * @return message
+     * @throws NoSuchMessageException 在任何locale中都找不到
+     * @see <a href="http://java.sun.com/j2se/1.3/docs/api/java/text/MessageFormat.html">java.text.MessageFormat</a>
+     */
+    String getMessage(String code, Object args[], Locale locale) throws NoSuchMessageException;
 
-	/**
-	 * <b>Using all the attributes contained within the <code>MessageSourceResolvable</code>
-	 * arg that was passed in (except for the <code>locale</code> attribute)</b>,
-	 * try to resolve the message from the <code>MessageSource</code> contained within the <code>Context</code>.<p>
-	 * NOTE: We must throw a <code>NoSuchMessageException</code> on this method since
-	 * at the time of calling this method we aren't able to determine if the <code>defaultMessage</code>
-	 * attribute is null or not.
-	 *
-	 * @param resolvable Value object storing 4 attributes required to properly resolve a message.
-	 * @param locale     Locale to be used as the "driver" to figuring out what message to return.
-	 * @return message Resolved message.
-	 * @throws NoSuchMessageException not found in any locale
-	 * @see <a href="http://java.sun.com/j2se/1.3/docs/api/java/text/MessageFormat.html">java.text.MessageFormat</a>
-	 */
-	String getMessage(MessageSourceResolvable resolvable, Locale locale) throws NoSuchMessageException;
+    /**
+     * <b>使用传入的<code>MessageSourceResolvable</code>arg中包含的所有属性
+     * (<code>locale</code>属性除外)</b>,
+     * 尝试从<code>Context</code>中包含的<code>MessageSource</code>解析消息.<p>
+     * 注意: 我们必须对此方法抛出<code>NoSuchMessageException</code>, 因为在调用此方法时,
+     * 我们无法确定<code>defaultMessage</code>属性是否为null.
+     *
+     * @param resolvable value对象, 存储正确解析消息所需的4个属性.
+     * @param locale     Locale用作"driver", 以确定要返回的消息.
+     * @return message Resolved message.
+     * @throws NoSuchMessageException 在任何locale中都找不到
+     * @see <a href="http://java.sun.com/j2se/1.3/docs/api/java/text/MessageFormat.html">java.text.MessageFormat</a>
+     */
+    String getMessage(MessageSourceResolvable resolvable, Locale locale) throws NoSuchMessageException;
 }
-
