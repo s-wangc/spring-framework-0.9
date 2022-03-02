fff2f4e811d5194ae2972aca97b74916ed623dde
wangc <1969877890@qq.com>
2022-03-02 21:04:02 +0800

    了解用来读取servlet配置的ServletConfigPropertyValues类, 该类读取servlet配置, 且是一个不可变类.

--- spring-web/src/com/interface21/web/servlet/ServletConfigPropertyValues.java
+++ spring-web/src/com/interface21/web/servlet/ServletConfigPropertyValues.java
@@ -16,8 +16,8 @@ import com.interface21.beans.PropertyValues;
 import com.interface21.util.StringUtils;
 
 /**
- * PropertyValues implementation created from ServetConfig parameters.
- * This class is immutable once initialized.
+ * 从ServletConfig参数创建的propertyvalues实现.
+ * 一旦初始化, 这个类就不可变.
  *
  * @author Rod Johnson
  */
@@ -25,32 +25,30 @@ class ServletConfigPropertyValues implements PropertyValues {
 
 	protected final Log logger = LogFactory.getLog(getClass());
 	/**
-	 * PropertyValues delegate. We use delegation rather than simply subclass
-	 * MutablePropertyValues as we don't want to expose MutablePropertyValues's
-	 * update methods. This class is immutable once initialized.
+	 * PropertyValues代理. 我们使用代理而不是简单地将MutablePropertyValues子类化,
+	 * 因为我们不想公开MutablePropertyValues的update方法. 初始化后, 此类是不可变的.
 	 */
 	private MutablePropertyValues mutablePropertyValues;
 
 	/**
-	 * Creates new PropertyValues object
+	 * 创建一个新的PropertyValues对象
 	 *
-	 * @param config ServletConfig we'll use to take PropertyValues from
-	 * @throws ServletException should never be thrown from this method
+	 * @param config 我们将使用的从中获取PropertyValues的ServletConfig
+	 * @throws ServletException 不应从此方法中抛出
 	 */
 	public ServletConfigPropertyValues(ServletConfig config) throws ServletException {
 		this(config, null);
 	}
 
 	/**
-	 * Creates new PropertyValues object
+	 * 创建一个新的PropertyValues对象
 	 *
-	 * @param config             ServletConfig we'll use to take PropertyValues from
-	 * @param requiredProperties array of property names we need, where
-	 *                           we can't accept default values
-	 * @throws ServletException if any required properties are missing
+	 * @param config             我们将使用的从中获取PropertyValues的ServletConfig
+	 * @param requiredProperties 我们需要的属性名称数组, 我们不能接受默认值
+	 * @throws ServletException 如果缺少任何必需的属性
 	 */
 	public ServletConfigPropertyValues(ServletConfig config, List requiredProperties) throws ServletException {
-		// Ensure we have a deep copy
+		// 确保我们有一个深拷贝副本
 		List missingProps = (requiredProperties == null) ? new ArrayList(0) : new ArrayList(requiredProperties);
 
 		mutablePropertyValues = new MutablePropertyValues();
@@ -63,7 +61,7 @@ class ServletConfigPropertyValues implements PropertyValues {
 			missingProps.remove(property);
 		}
 
-		// Fail if we are still missing properties
+		// 如果我们仍然找不到(必须)属性, 则失败
 		if (missingProps.size() > 0) {
 			throw new ServletException("Initialization from ServletConfig for servlet '" + config.getServletName() + "' failed: the following required properties were missing -- (" +
 					StringUtils.collectionToDelimitedString(missingProps, ", ") + ")");
@@ -76,34 +74,32 @@ class ServletConfigPropertyValues implements PropertyValues {
 
 
 	/**
-	 * Return an array of the PropertyValue objects
-	 * held in this object.
+	 * 返回此对象中包含的PropertyValue对象的数组.
 	 *
-	 * @return an array of the PropertyValue objects
-	 * held in this object.
+	 * @return 此对象中包含的PropertyValue对象的数组.
 	 */
 	public PropertyValue[] getPropertyValues() {
-		// We simply let the delegate handle this
+		// 我们只是让delegate来处理这个问题
 		return mutablePropertyValues.getPropertyValues();
 	}
 
 	/**
-	 * Is there a propertyValue object for this property?
+	 * 此属性是否有propertyValue对象?
 	 *
-	 * @param propertyName name of the property we're interested in
-	 * @return whether there is a propertyValue object for this property?
+	 * @param propertyName 我们感兴趣的属性名称
+	 * @return 此属性是否有propertyValue对象?
 	 */
 	public boolean contains(String propertyName) {
 		return mutablePropertyValues.contains(propertyName);
 	}
 
 	public PropertyValue getPropertyValue(String propertyName) {
-		// Just pass it to the delegate...
+		// 把它传给delegate...
 		return mutablePropertyValues.getPropertyValue(propertyName);
 	}
 
 	public PropertyValues changesSince(PropertyValues old) {
-		// Just pass it to the delegate...
+		// 把它传给delegate...
 		return mutablePropertyValues.changesSince(old);
 	}
 
